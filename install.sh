34c13c71b8bf3311bde223970e9042e23b89528e
Mohamed <Ethical.Hacker720@gmail.com>
2018-11-06 08:45:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 install() {
  # configure the repository file
- echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+ echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
  # update outdated 
  gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
